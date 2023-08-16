2f2dd8e701e9fa08415ae554e78b1063a16b7c78
kubernetes <mihaicostin342@gmail.com>
2023-08-16 09:18:22 +0000

    Private messaging

--- discussions/discussions.py
+++ discussions/discussions.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 users = redis.Redis(host = os.environ["USERS"], port=6379, decode_responses=True)
 sessions = redis.Redis(host=os.environ["SESSIONS"], port=6381, decode_responses=True)
-messages = redis.Redis(host=os.environ["MESSAGES"], port=6380, decode_responses=True)
+messages = redis.Redis(host=os.environ["MESSAGES"], port=6380, decode_responses=True, db=0)
+pms = redis.Redis(host=os.environ["MESSAGES"], port=6380, decode_responses=True, db=1)
 
 @app.route("/messages", methods=["GET", "POST"])
 def messages_sent():
@@ -23,8 +24,42 @@ def messages_sent():
                 "date" : res[2]
             } 
             mess_list.append(mess_instance)
+    newlist = sorted(mess_list, key=lambda d: d["date"])
+    newlist.reverse()
     res = {
         "code" : 0,
-        "messages" : mess_list
+        "messages" : newlist
     }
     return json.dumps(res)
+
+@app.route("/messages/pms", methods=["POST"])
+def pms_received():
+    content = request.json
+    uname = content["username"]
+    token = content["token"]
+    token_check = sessions.hget(uname, "token")
+    private_msgs = []
+    if str(token) == token_check:
+        for key in pms.keys():
+            if str(key) != "message_id":
+                val = pms.hmget(key, "username", "destination", "date", "message")
+                if val[1] == uname : 
+                    mess_instance = {
+                        "sender" : val[0],
+                        "date" : val[2],
+                        "message" : val[3]
+                    }
+                    private_msgs.append(mess_instance)
+        sorted_msgs = sorted(private_msgs, key=lambda d: d["date"])
+        private_msgs.reverse()
+        res = {
+            "code" : 0,
+            "messages" : private_msgs
+        }
+        return json.dumps(res) 
+    else:
+        res = {
+            "code" : -1,
+            "Message" : "User is not logged in"
+        }
+        return json.dumps(res)
--- post/post.py
+++ post/post.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 users = redis.Redis(host = os.environ["USERS"], port=6379, decode_responses=True)
 sessions = redis.Redis(host=os.environ["SESSIONS"], port=6381, decode_responses=True)
-messages = redis.Redis(host=os.environ["MESSAGES"], port=6380, decode_responses=True)
+messages = redis.Redis(host=os.environ["MESSAGES"], port=6380, decode_responses=True, db=0)
+pms = redis.Redis(host=os.environ["MESSAGES"], port=6380, decode_responses=True, db=1)
 
 @app.route("/post", methods=["POST"])
 def post_message():
@@ -41,5 +42,35 @@ def post_message():
         res = {
             "code" : -1,
             "message" : "Incorrect user token!"
+        }
+        return json.dumps(res)
+
+@app.route("/post/pm", methods=["POST"])
+def private_message():
+    content = request.json
+    uname = content["username"]
+    dest = content["destination"]
+    msg = content["message"]
+    if dest in users.keys():
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        pms.incr("message_id")
+        pms.hmset(pms.get("message_id"),
+            {
+                "username" : uname,
+                "destination" : dest,
+                "date" : dt_string,
+                "message" : msg
+            }
+        )
+        res = {
+            "code" : 0,
+            "message" : "Message successfully uploaded"
+        }
+        return json.dumps(res)
+    else:
+        res = {
+            "code" : -1,
+            "message" : "This user doesn't exist"
         }
         return json.dumps(res)
